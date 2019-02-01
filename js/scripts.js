c85648308e0a17c909c59e4263a878cc9e588e36
Snulu <Nnabwami@gmail.com>
2019-02-01 11:34:06 -0800

    get all selected toppings from the checkbox.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,31 @@
 function PizzaJoint() {
-  this.base = "";
-  this.toppings = [];
-  this.price = 0;
+  this.base = "",
+  this.price = 0,
+  this.toppings = []
 }
 
-PizzaJoint.prototype.setPrice = function (){
+PizzaJoint.prototype.addToppings =
+function (topping) {
+  this.toppings.push(base)
+}
+
+PizzaJoint.prototype.setPrice =
+function (base){
   var basePrice = 10;
   var toppingPrice = 1;
   this.price = basePrice + this.toppings.length*toppingPrice;
 }
 
-
 $(document).ready(function() {
   $("form#new-pizza").submit(function(event) {
     event.preventDefault();
 
-        var pizzaBase = $("input:radio[name=pizza-base]:checked").val();
-        var pizzaToppings = $("input:checkbox[name=toppings]:checked").val();
+    var pizzaBase = $("input:radio[name=pizza-base]:checked").val();
+    console.log(pizzaBase);
 
-        console.log(pizzaBase);
-        console.log(pizzaToppings);
+    var pizzaToppings = $("input:checkbox[name=toppings]:checked").each(function(index){
+      console.log(index+' checkbox has value ' +$(this).val());
+    });
+
   })
 })
